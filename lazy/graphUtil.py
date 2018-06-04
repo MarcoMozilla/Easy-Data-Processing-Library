dac6fd48af0accb023e8490d5baff7412044cc5f
Sitao Wang <sitaowang1998@outlook.com>
2018-06-04 14:04:51 -0400

    Added bar function to plot the bar graph

--- lazy/graphUtil.py
+++ lazy/graphUtil.py
@@ -1,9 +1,15 @@
+#import matplotlib.pyplot as plt
+#from mpl_interaction import PanAndZoom as Pan
+
+
 class GraphUtil:
 
-    # Imports matplotlib.
-    # Returns True if succeed, prints message and returns false otherwise.
     @staticmethod
     def has_matplotlib():
+        """ Imports matplotlib.
+        :return: Returns True if succeed, prints message and returns false otherwise.
+        :rtype: bool
+        """
         try:
             import matplotlib.pyplot as plt
         except ImportError:
@@ -13,11 +19,35 @@ class GraphUtil:
             return True
 
     @staticmethod
-    def has_mpi_interaction():
+    def has_mpl_interaction():
+        """ Imports MplInteraction.
+        :return: Returns True if succeed, prints message and returns false otherwise.
+        :rtype: bool
+        """
         try:
-            import MpiInteraction as Mpi
+            from .mpl_interaction import PanAndZoom as Pan
         except ImportError:
             print("Please install MpiInteraction.")
             return False
         else:
             return True
+
+    @staticmethod
+    def bar(label, value, label_name=None, value_name=None, title=None):
+        """Draws the bar diagram.
+        No graph is drawn if matplotlib is not installed.
+        The graph cannot be dragged and zoomed by mouse if MpiInteraction not installed.
+        """
+        if not GraphUtil.has_matplotlib():
+            return
+        fig, ax = plt.subplots()
+        ax.bar(label, value)
+        if not label_name is None:
+            plt.xlabel(label_name)
+        if not value_name is None:
+            plt.ylabel(value_name)
+        if not title is None:
+            plt.suptitle(title)
+        if not GraphUtil.has_mpl_interaction():
+            pan_zoom = Pan(fig)
+        plt.show()
